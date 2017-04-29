80dfc2a27621dcdb1d3602107552c9e38c4677e3
adsalgue <adsalgue@ucsc.edu>
2017-04-29 15:14:07 -0700

    Main and text page
    Added main page and text edit page

--- GhostEchoes/app/src/main/java/com/example/ghostechoes/ghostechoes/Echo_Input_Text.java
+++ GhostEchoes/app/src/main/java/com/example/ghostechoes/ghostechoes/Echo_Input_Text.java
@@ -2,7 +2,6 @@ package com.example.ghostechoes.ghostechoes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -10,20 +9,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
-    private final String LOG_TAG = "Ghost_Echoes";
+public class Echo_Input_Text extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_echo__input__text);
     }
 
-    // Enter the application and change activity to MapActivity (or whatever it is called)
-    public void onClickEnterApp(View V){
-        Log.d(LOG_TAG,"Hitting the Enter Button");
-        Intent intent = new Intent(this, Echo_Input_Text.class);
+    public void onClickPostEcho(View v){
+        Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }

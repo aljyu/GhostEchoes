bb02fa6758cf2912773362e856c2221bedaa9de9
adsalgue <adsalgue@ucsc.edu>
2017-06-04 21:31:08 -0700

    New class to store database

---
+++ GhostEchoes/app/src/main/java/com/example/ghostechoes/ghostechoes/getEchoes.java
@@ -0,0 +1,14 @@
+package com.example.ghostechoes.ghostechoes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class getEchoes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_echoes);
+    }
+}

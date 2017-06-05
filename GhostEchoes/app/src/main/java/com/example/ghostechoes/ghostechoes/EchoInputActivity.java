764ab3654cbca5a8e042fc8bb934aa3759129345
adsalgue <adsalgue@ucsc.edu>
2017-06-05 00:25:19 -0700

    Resolving file error issues

--- GhostEchoes/app/src/main/java/com/example/ghostechoes/ghostechoes/EchoInputActivity.java
+++ GhostEchoes/app/src/main/java/com/example/ghostechoes/ghostechoes/EchoInputActivity.java
@@ -169,7 +169,7 @@ public class EchoInputActivity extends AppCompatActivity {
         // @TODO - Store to Database Photo, Text, Location
         // Should only go to echo when location can be retrieved
 
-        Intent intent = new Intent(this, Getechoes.class);
+        Intent intent = new Intent(this, GetEchoes.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
---
+++ GhostEchoes/app/src/main/java/com/example/ghostechoes/ghostechoes/GetEchoes.java
@@ -0,0 +1,91 @@
+package com.example.ghostechoes.ghostechoes;
+
+import android.app.Activity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
+
+public class GetEchoes extends AppCompatActivity {
+    // Buttons, Views
+    Button btn_map;                // Map button
+    // Request queue
+    //RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_echoes);
+        // Clickable Objects
+        //btn_map = (Button) findViewById(R.id.button3);
+        // Request queue
+        //queue = Volley.newRequestQueue(this);
+        // Store messages to the database
+        //storeMessage();
+    }
+
+    public void storeMessage() {
+		/*
+        final TextView msg = (TextView) findViewById(R.id.textView3);
+        // Instantiate the RequestQueue
+        String url = "darkfeather2.pythonanywhere.com/get_data";
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String output = response.toString();
+                        msg.setText(output);
+                        //Log.d(LOG_TAG, "Received: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //Log.d(LOG_TAG, error.toString());
+                    }
+                });
+        queue.add(jsObjRequest);
+		*/
+    }
+
+    public void goToMap(View v) {
+        // Should only go to echo when location can be retrieved
+        Intent intent = new Intent(this, GoogleMapsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+}
